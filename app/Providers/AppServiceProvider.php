6df8c982f9432810dab5c7c3c08063b72b0cafe5
Junio Santos <juniops@apoioprodesp.sp.gov.br>
2019-09-01 15:54:13 -0300

    remove airbrake/phpbrake install rollbar/rollbar:~1.5

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use \Rollbar\Rollbar;
+use \Rollbar\Payload\Level;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +15,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        Rollbar::init(
+            array(
+                'access_token' => '9e8cf43097b3483c94b6617e4905a32b',
+                'environment' => 'production'
+            )
+        );
     }
 
     /**
